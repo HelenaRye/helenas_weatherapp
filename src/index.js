48178699c94bc343b44d573091dc474182e66047
HelenaRye <78907672+HelenaRye@users.noreply.github.com>
2021-02-12 19:42:35 +0100

    unit links added in js

--- src/index.js
+++ src/index.js
@@ -64,14 +64,19 @@ function showTemperature(response) {
 
 function showFahrenheitTemperature (event) {
   event.preventDefault();
-  let fahrenheitTemperature = (celsiusTemperature * 9)/5+32;
   let temperatureElement = document.querySelector(".temperature-number");
+  
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9)/5+32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 
 }
 
 function showCelsiusTemperature (event) {
   event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   temperatureEelement.innerHTML = Math.round(celsiusTemperature);
 
 }
